fa905f39f1af4ef7f4cf94909249cfbbadf8c98f
anandsathya80 <anandsathya80@gmail.com>
2024-08-15 14:18:15 +0700

    feat: controller: doctor controller

---
+++ app/Http/Controllers/doctors/DoctorController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\doctors;
+
+use App\Http\Controllers\Controller;
+use App\Models\Doctor;
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $doctor = Doctor::all();
+        return response()->json($doctor);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $doctor = Doctor::create([
+            'name' => $request->input('name'),
+            'phone' => $request->input('phone'),
+            'address' => $request->input('address'),
+            'email' => $request->input('email'),
+            'division_id' => $request->input('division_id'),
+            'sex_id' => $request->input('sex_id'),
+            'doctor_type_id' => $request->input('doctor_type_id'),
+        ]);
+        return response()->json([$doctor, 'created successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
