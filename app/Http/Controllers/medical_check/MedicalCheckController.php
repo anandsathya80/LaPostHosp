7ff42703d05888520d0eff1da4602c0f09552f99
anandsathya80 <anandsathya80@gmail.com>
2024-08-16 15:15:03 +0700

    feat: controller fro medical check

---
+++ app/Http/Controllers/medical_check/MedicalCheckController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\medical_check;
+
+use App\Http\Controllers\Controller;
+use App\Models\MedicalCheck;
+use Illuminate\Http\Request;
+
+class MedicalCheckController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $medicalCheck = MedicalCheck::all();
+        return response()->json($medicalCheck);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $medicalCheck = MedicalCheck::create([
+            
+        ]);
+
+        return response()->json($medicalCheck, 'created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
