8201c51eed2fd4f3ccbdf5be71e396fd0d4d8361
anandsathya80 <anandsathya80@gmail.com>
2024-08-19 09:14:26 +0700

    fix: reapir sexes table name

--- database/migrations/2024_07_14_014148_create_sexs_table.php
+++ database/migrations/2024_07_14_014148_create_sexs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sexs', function (Blueprint $table) {
+        Schema::create('sexes', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('sex');
             $table->timestamps();
