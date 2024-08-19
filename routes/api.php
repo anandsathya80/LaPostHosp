386f1d77595f9d7ddf5fa6b39335d22056c39e55
anandsathya80 <anandsathya80@gmail.com>
2024-08-19 09:14:08 +0700

    feat: routes for api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\divisions\DivisionController;
+use App\Http\Controllers\doctors\DoctorController;
+use App\Http\Controllers\nurses\NurseController;
+use App\Http\Controllers\patients\PatientController;
+use App\Http\Controllers\static_type\BloodTypeController;
+use App\Http\Controllers\static_type\SexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +20,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// sex
+Route::get('/sexs', [SexController::class, 'index']);
+Route::post('/storeSexs', [SexController::class, 'store']);
+// sex
+
+// division
+Route::get('/divisions', [DivisionController::class, 'index']);
+Route::post('/storeDivisions', [DivisionController::class, 'store']);
+// division
+
+// doctors
+Route::get('/doctors', [DoctorController::class, 'index']);
+Route::post('/storeDoctors', [DoctorController::class, 'store']);
+// doctors
+
+// nurses
+Route::get('/nurses', [NurseController::class, 'index']);
+Route::post('/storeNurses', [NurseController::class, 'store']);
+// nurses
+
+// bloodType
+Route::get('/bloodTypes', [BloodTypeController::class, 'index']);
+Route::post('/storeBloodTypes', [BloodTypeController::class, 'store']);
+// bloodType
+
+// patients
+Route::get('/patients', [PatientController::class, 'index']);
+Route::post('/storePatients', [PatientController::class, 'store']);
+// patients
